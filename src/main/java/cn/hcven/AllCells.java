127c0a19504d9f26739d1a3745259c39b1413e3b
HC <zhanghaozjk@outlook.com>
2017-12-24 20:21:58 +0800

    Basic functions but not at the same time

--- src/main/java/cn/hcven/AllCells.java
+++ src/main/java/cn/hcven/AllCells.java
@@ -3,7 +3,7 @@ package cn.hcven;
 public class AllCells {
     private int length;
     private int width;
-    Cells[][] allcells;
+    private Cells[][] allcells;
     public AllCells(){
         this.length = 0;
         this.width = 0;
@@ -18,6 +18,15 @@ public class AllCells {
             }
         }
     }
+    public boolean setAlive(int x, int y){
+        try {
+            allcells[x][y].setAlive();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
     public boolean print(){
         for (int i = 0; i < width; i++){
             for (int j = 0; j < length; j++){
@@ -29,6 +38,58 @@ public class AllCells {
                 }
             }
             System.out.println("");
+        }
+        return true;
+    }
+
+    int summer(int i, int j){
+        int sum = -1;
+        int x = i - 1;
+        int y = j - 1;
+        int m = 3, n = 3;
+        while (m > 0){
+            while (n > 0){
+                try{
+                    if (x < 0 || y < 0) {
+                        n--;
+                        y++;
+                        continue;
+                    } else if (x > length || y >= width){
+                        break;
+                    } else if (allcells[x][y].isAlive()) {
+                        sum++;
+                    }
+                } catch (ArrayIndexOutOfBoundsException e){};
+                y++;
+                n--;
+            }
+            y = j - 1;
+            x++;
+            n = 3;
+            m--;
+        }
+
+        return sum;
+    }
+
+    public boolean flush(){
+        int sum = 0;
+        for (int i = 0; i< length; i++){
+            for (int j = 0; j < width; j++){
+                if (allcells[i][j].isAlive()){
+                    sum = summer(i, j);
+                    if (sum == 0 || sum == 1) {
+                        allcells[i][j].setDie();
+                    } else if (sum > 3) {
+                        allcells[i][j].setDie();
+                    }
+                } else {
+                    sum = summer(i, j);
+                    if (sum == 2) {
+                        allcells[i][j].setAlive();
+                    }
+                }
+            }
         }
         return true;
     }
--- src/main/java/cn/hcven/Main.java
+++ src/main/java/cn/hcven/Main.java
@@ -1,36 +1,22 @@
 package cn.hcven;
 
-import cn.hcven.Cells;
-
 public class Main {
 
     public static void main(String[] args) {
-        Cells[][] allcells = new Cells[64][64];
-        for (int i = 0; i < 64; i++) {
-            for (int j = 0; j < 64; j++){
-                allcells[i][j] = new Cells();
-            }
-        }
-        allcells[13][23].setAlive();
-        allcells[42][13].setAlive();
-        allcells[10][45].setAlive();
-        allcells[17][12].setAlive();
-        allcells[10][20].setAlive();
+        AllCells cells = new AllCells(4,4);
 
+        cells.setAlive(3, 2);
+        cells.setAlive(2, 3);
+        cells.setAlive(2, 2);
+        cells.setAlive(3, 3);
+        cells.setAlive(1, 3);
+
+        System.out.println("==========Init==========");
+        cells.print();
         while (true) {
             System.out.println("============NEW STATUS===========");
-
-            for (int i = 0; i < 64; i++){
-                for (int j = 0; j < 64; j++){
-                    if (allcells[i][j].isAlive()){
-                        System.out.print('*');
-
-                    } else {
-                        System.out.print(' ');
-                    }
-                }
-                System.out.println("");
-            }
+            cells.flush();
+            cells.print();
 
             try {
                 Thread.sleep(1000);
